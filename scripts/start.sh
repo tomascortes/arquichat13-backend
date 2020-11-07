12a0f1a7fbdc1978aa1da475db23525d5593feec
alejandro <aphillips@uc.cl>
2020-11-07 17:49:49 -0300

    ubuntu en vez de ec2user

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo docker-compose -f /home/ec2-user/arquichat/docker-compose.yml up -d
+sudo docker-compose -f /home/ubuntu/arquichat/docker-compose.yml up -d
 
 # sudo docker-compose -f /home/ec2-user/arquichat/docker-compose.production.yml up -d
