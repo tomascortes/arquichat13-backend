a2db4ac61dc16346d2b7b6c6463e26633aa180c7
tomascortes <ticortes@uc.cl>
2020-11-07 17:01:24 -0300

    paths arreglados denuevo

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,4 @@
 pwd=$( aws ecr get-login-password )
 docker container stop $(docker container ls -aq)
 docker login -u AWS -p $pwd https://374139594143.dkr.ecr.us-east-2.amazonaws.com
-docker pull 374139594143.dkr.ecr.us-east-2.amazonaws.com/MYAPP
+docker pull 374139594143.dkr.ecr.us-east-2.amazonaws.com/arquichat
