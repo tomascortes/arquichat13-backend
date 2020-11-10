720473741b8b30dfc7962497ec588438e90aa0e6
tomascortes <ticortes@uc.cl>
2020-11-10 00:05:10 -0300

    wea del nico

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
 sudo docker-compose -f /home/ubuntu/arquichat/docker-compose.production.yml up -d
+
+
 
 # sudo docker-compose -f /home/ec2-user/arquichat/docker-compose.production.yml up -d
