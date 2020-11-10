499700a6ea38f2b13666dab9a0ab9006419a5c45
Ubuntu <ubuntu@ip-172-31-15-86.us-east-2.compute.internal>
2020-11-10 08:07:35 +0000

    aaaaa

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+cd  /home/ubuntu/arquichat/
+printf 'yes\2' | ./init-letsencrypt.sh
 sudo docker-compose -f /home/ubuntu/arquichat/docker-compose.production.yml up -d
 
+
+
 # sudo docker-compose -f /home/ec2-user/arquichat/docker-compose.production.yml up -d
+
