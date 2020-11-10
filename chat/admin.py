9ee1b6ec8185be012c0395e0eb790b7e593f25ad
alejandro <aphillips@uc.cl>
2020-11-10 02:08:04 -0300

    codigo santi

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import ChatRoom 
+
+from .models import ChatRoom
+
 
 admin.site.register(
     ChatRoom,
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
 sudo docker-compose -f /home/ubuntu/arquichat/docker-compose.production.yml down
-
 sudo docker stop $(docker ps -a -q)
