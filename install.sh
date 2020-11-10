43da4f3ab6341fb05817257772aaf474682af0a0
tomascortes <ticortes@uc.cl>
2020-11-09 23:13:52 -0300

    cambios al compose

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt install python3-pip
-sudo pip install awscli 
-pwd=$( aws ecr get-login-password )
-sudo service docker start
-# sudo docker container stop $(docker container ls -aq)
-sudo docker login -u AWS -p $pwd https://374139594143.dkr.ecr.us-east-2.amazonaws.com/arquichat
-sudo docker pull 374139594143.dkr.ecr.us-east-2.amazonaws.com/arquichat
--- stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo docker-compose -f /home/ubuntu/arquichat/scripts/docker-compose.production.yml down
-sudo docker stop $(docker ps -a -q)
