4d5e49279547d5dee256339a80dfe0f06036577d
tomascortes <ticortes@uc.cl>
2020-11-20 17:52:45 -0300

    test con nico

--- chat/tests.py
+++ chat/tests.py
@@ -18,4 +18,4 @@ class ChatTests(TestCase):
     def test_new_chat_room(self):
         response = self.client.get(reverse('room', args=['test_room']))
         response = json.loads(response.content)
-        self.assertEqual(response, {'alias': 'anon', 'room_topic': 'test_room', 'messages': []})
+        self.assertEqual(response, {'alias': 'anon', 'room_topic': 'test_room', 'messages': []}) 
