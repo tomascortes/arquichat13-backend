61d0fe9238d4fb8dc52a5c651de926e145dec18b
alejandro <aphillips@uc.cl>
2020-11-21 09:49:25 -0300

    lets encrypt

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo ./init-letsencrypt.sh
 sudo docker-compose -f /home/ubuntu/arquichat/docker-compose.production.yml up -d
-
+sudo ./init-letsencrypt.sh
 
 
 # sudo docker-compose -f /home/ec2-user/arquichat/docker-compose.production.yml up -d
