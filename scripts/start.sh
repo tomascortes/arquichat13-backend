1a0d2c992e3372cc0e3d74e7956cba14ab9964fd
alejandro <aphillips@uc.cl>
2020-11-21 10:30:54 -0300

    solo un letsencrypt

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo ./init-letsencrypt.sh
 sudo docker-compose -f /home/ubuntu/arquichat/docker-compose.production.yml up -d
-sudo ./init-letsencrypt.sh
 
 
 # sudo docker-compose -f /home/ec2-user/arquichat/docker-compose.production.yml up -d
